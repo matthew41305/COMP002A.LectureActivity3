55856f73bb79a6b4763b6dfedbb5080988983005
Matthew Hudson <matthew41305@gmail.com>
2024-10-30 20:26:48 -0700

    added comments

--- module-03/function.js
+++ module-03/function.js
@@ -47,5 +47,53 @@ printHelloWorld5("Matthew", "Jonathan,", "Humberto", "Jerry", "Jovanni");
 let someVariable = "";
 console.log('hello, ' + someVariable); //single quotes
 console.log("hello, " + someVariable); //double quotes
-console.log('hello, ${someVariable}'); // backticks with string interpolation
+console.log(hello, ${someVariable}); // backticks with string interpolation
 
+/*****************************************************************************************************/
+// function calling a function
+function printHelloWorld6() {
+    console.log("This is printHelloWorld6");
+
+function printHelloWorld7() {
+    console.log("This is printHelloWorld7");
+    printHelloWorld6(); // calling a function inside another function
+
+    }
+}
+
+printHelloWorld7(); // prints both "This is printHelloWorld(7))" & "This is printHelloWorld6()"
+printHelloWorld6(); // prints just "This is printHelloWorld(6)"
+
+
+/************************************************************/
+// recursion
+const factorial = n => {
+    if (n <= 1) {
+        return 1; // the base "case"
+    }
+
+    return n * factorial(n - 1); //recursive call
+};
+
+console.log(factorial(5));
+
+
+/************************************************************/
+// no return vs return values functions
+
+// functions without a return value
+function sum(num1, num2) {
+    console.log(num1 + num2); //prints the sum of num1 and num2
+}
+
+// function with a return value
+function difference(num1, num2) {
+    return num1 - num2; // returns the difference between num1 & num2 but it does not print it
+}
+
+sum (2, 10); // since this function did not return a value it cannot be manipulated further
+
+// since the difference function returns a value, it CAN be manipulated further
+
+let addTentoDifference = difference(10, 5) + 10;
+console.log(addTentoDifference);
